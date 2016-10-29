40f4d4a732c6a7ae22579c115999faa3da59ba64
spbleue <sbaii.mouad@gmail.com>
2016-10-29 10:55:33 +0100

    socket
    multi listen socket

--- nodeserverclient/app.js
+++ nodeserverclient/app.js
@@ -1,7 +1,9 @@
+
 var net = require('net');
 
 var HOST = '127.0.0.1';
 var PORT = 6969;
+var PORT2 = 6967;
 
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
@@ -26,5 +28,27 @@ net.createServer(function(sock) {
     });
     
 }).listen(PORT, HOST);
+
+
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT2, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);

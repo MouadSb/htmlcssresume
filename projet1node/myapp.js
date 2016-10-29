b9bb0e39a9c0ba466299ef5936d43f4ff9643237
spbleue <sbaii.mouad@gmail.com>
2016-10-29 10:47:04 +0100

    socket
    socket listenning

---
+++ projet1node/myapp.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var session = require('cookie-session');
+var bodyParser = require('body-parser');
+var urlencodedParser  = bodyParser.urlencoded({extended : false});
+var app = express();
+
+
+app.use(session({secret: 'todotopsecret'}))
+
+.use(function(req,res,next){
+	if(typeof(req.session.todolist) =='undefined'){
+		req.session.todolist = [];
+	}
+	next();
+})
+
+
+.get("/todo",function(req,res){
+	res.render('todo.ejs',{todolist : req.session.todolist});
+})
+
+.get("/:test",function(req,res){
+	console.log(req.params.test);
+})
+
+.post("/todo/ajouter/",urlencodedParser,function(req,res){
+	if(req.body.newtodo != ''){
+		req.session.todolist.push(req.body.newtodo);
+	}
+	res.redirect('/todo');
+})
+
+.get("/todo/delete/:id",function(req,res){
+	if(req.params.id != ''){
+		req.session.todolist.splice(req.params.id, 1);
+	}
+	res.redirect('/todo');
+})
+
+.use(function(req,res,next){
+	res.redirect('/todo');
+})
+
+.listen(8082);
